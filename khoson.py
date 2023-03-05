d893dd5564deaad10a3a25803071f7a3cb46d114
ttfkhoson <104403361+ttfkhoson@users.noreply.github.com>
2023-03-05 12:07:01 +0700

    Update khoson.py

--- khoson.py
+++ khoson.py
@@ -62,8 +62,8 @@ def push(df,gc,sheet):
     sheet=gc.open("Kho sơn - DS đặt hàng").worksheet(sheet)
     data=gd.get_as_dataframe(sheet)
     new_df=data.append(df)
-    new_df=new_df.dropna()
-    gd.set_with_dataframe(sheet,new_df)
+    newdf=new_df.loc[new_df['Khối lượng sơn'] >0]
+    gd.set_with_dataframe(sheet,newdf)
     st.success('xong')
 
 
